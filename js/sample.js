4845950b57895578790d4087aff536bbaa8ba1cc
Woosuk Seo <seow@uw.edu>
2016-05-10 01:19:47 -0700

    Rename main.js and data as sample

--- js/sample.js
+++ js/sample.js
@@ -1,7 +1,6 @@
 
 var myChart = BubbleChart();
 
-
 // var myChart = BubbleChart().diameter(1000)
 // 							.padding(2.5)
 //                             .format(",d")
@@ -9,7 +8,7 @@ var myChart = BubbleChart();
 //                             .height(1000)
 //                             .color(d3.scale.category20c());
 
-d3.json("flare.json",function (error, root) {
+d3.json("sampleData.json",function (error, root) {
 	var chartWrapper = d3.select('#my-div')
 	                .datum(root) 
 	                .call(myChart);
